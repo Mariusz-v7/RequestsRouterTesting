49e8f8c3ca0d52471b083ae76521c72a13234958
Mariusz Gęzicki <mariusz.v7@gmail.com>
2017-05-13 16:41:21 +0200

    Add LocalSession helper class to set session for test purposes

---
+++ src/main/java/pl/mrugames/commons/router/sessions/LocalSession.java
@@ -0,0 +1,7 @@
+package pl.mrugames.commons.router.sessions;
+
+public class LocalSession {
+    public static void setLocalSession(Session session) {
+        Session.setLocalSession(session);
+    }
+}
