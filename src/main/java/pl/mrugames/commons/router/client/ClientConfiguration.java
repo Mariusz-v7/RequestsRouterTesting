3769c6fb333ed2b9b18d16078dad676f2da3e6cc
Mariusz Gęzicki <mariusz.v7@gmail.com>
2018-02-11 13:20:19 +0100

    Add session to client

--- src/main/java/pl/mrugames/commons/router/client/ClientConfiguration.java
+++ src/main/java/pl/mrugames/commons/router/client/ClientConfiguration.java
@@ -2,12 +2,13 @@ package pl.mrugames.commons.router.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import pl.mrugames.commons.router.sessions.Session;
 
 @Configuration
 public class ClientConfiguration {
     @Bean
     public Client client(TestConnector testConnector) {
-        return new Client(30000, testConnector);
+        return new Client(30000, testConnector, new Session());
     }
 
 }
